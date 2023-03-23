e2132a3db39dbac5887d4c163df8434f1e1ead4a
dsplayerX <dumindu0043@gmail.com>
2023-03-23 11:02:44 +0530

    added better styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import {
   Heading,
   Container,
   Text,
-  Input,
+  Flex,
+  Textarea,
   Button,
   Wrap,
   Stack,
@@ -12,6 +13,8 @@ import {
   SkeletonCircle,
   SkeletonText,
   Select,
+  SimpleGrid,
+  Box,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
@@ -46,106 +49,170 @@ const App = () => {
 
   return (
     <ChakraProvider>
-      <Container maxW="xl" mt="50px" mb="100px">
-        <Heading as="h1" mb="5">
-          Stable Diffusion Web App
-        </Heading>
-        <Text fontSize="xl" mb="10">
-          <Link
-            href={
-              "https://huggingface.co/dreamlike-art/dreamlike-diffusion-1.0"
-            }
-            isExternal
-            textDecoration="underline"
-          >
-            Dreamlike Diffusion v1 Model
-          </Link>
-          <br />
-          <Link
-            href={"https://huggingface.co/stabilityai/stable-diffusion-2"}
-            isExternal
-            textDecoration="underline"
-          >
-            Stable Diffusion v2 Model
-          </Link>
-          <br />
-          <Link
-            href={"https://huggingface.co/runwayml/stable-diffusion-v1-5"}
-            isExternal
-            textDecoration="underline"
-          >
-            Stable Diffusion v1.5 Model
-          </Link>
-          <br />
-          <Link
-            href={"https://huggingface.co/nousr/robo-diffusion"}
-            isExternal
-            textDecoration="underline"
-          >
-            Robo Diffusion
-          </Link>
-          <br />
-          <Link
-            href={"https://huggingface.co/nitrosocke/mo-di-diffusion"}
-            isExternal
-            textDecoration="underline"
-          >
-            Mo Di Diffusion Model
-          </Link>
-          <br />
-          <Link
-            href={"https://huggingface.co/hakurei/waifu-diffusion"}
-            isExternal
-            textDecoration="underline"
-          >
-            Waifu Diffusion Model
-          </Link>
-          <br />
-        </Text>
-
-        <Stack direction="row" mb="4">
-          <Select value={model} onChange={handleModelChange} width={"200px"}>
-            <option value="dreamlike_model">Dream-like Model v1.0</option>
-            <option value="stable_diffusion_2_model">
-              Stable Diffusion v2 Model
-            </option>
-            <option value="stable_diffusion_v1-5">Stable Diffusion v1.5</option>
-            <option value="robo_diffusion_2">Robo Diffusion 2</option>
-            <option value="mo_di_diffusion">Mo Di Diffusion</option>
-            <option value="waifu_diffusion">Waifu Diffusion</option>
-          </Select>
-          <Button onClick={(e) => selectmodel(model)} colorScheme={"red"}>
-            Apply
-          </Button>
-        </Stack>
-
-        <Wrap spacing="4" mb="10">
-          <Input
-            value={prompt}
-            onChange={(e) => updatePrompt(e.target.value)}
-            width={"350px"}
-            variant="filled"
-            placeholder="Enter your prompt here..."
-          />
-          <Button onClick={(e) => generate(prompt)} colorScheme={"red"}>
-            Dream
-          </Button>
-        </Wrap>
-
-        {loading ? (
-          <Stack>
-            <SkeletonCircle size="20" />
-            <SkeletonText mt="4" noOfLines={5} spacing="4" />
+      <Heading
+        as="h1"
+        mb="5"
+        fontSize={"42"}
+        textAlign="center"
+        color={"red"}
+        margin={"20px 0"}
+      >
+        Stable Diffusion Web App
+      </Heading>
+      <Heading
+        as="h1"
+        mb="5"
+        fontSize={"20"}
+        textAlign="center"
+        color={"grey"}
+        marginTop={"-25px"}
+      >
+        by{" "}
+        <Link
+          href={"https://github.com/dsplayerX"}
+          isExternal
+          textDecoration="none"
+        >
+          dsplayerX
+        </Link>
+      </Heading>
+      <SimpleGrid columns={{ base: 1, md: 2, md: 2 }} gap={5}>
+        <Box
+          width={"600px"}
+          height={"600px"}
+          margin="20px auto"
+          // backgroundColor={"red"}
+          padding={"20px"}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Stack direction="row" mb="4">
+            <Wrap spacing="4" mb="10">
+              <Select
+                value={model}
+                onChange={handleModelChange}
+                width={"350px"}
+              >
+                <option value="dreamlike_model">Dream-like v1.0</option>
+                <option value="stable_diffusion_2_model">
+                  Stable Diffusion v2
+                </option>
+                <option value="stable_diffusion_v1-5">
+                  Stable Diffusion v1.5
+                </option>
+                <option value="robo_diffusion_2">Robo Diffusion 2</option>
+                <option value="mo_di_diffusion">Mo Di Diffusion</option>
+                <option value="waifu_diffusion">Waifu Diffusion</option>
+              </Select>
+              <Button onClick={(e) => selectmodel(model)} colorScheme={"red"}>
+                Apply
+              </Button>
+            </Wrap>
           </Stack>
-        ) : image ? (
-          <Image
-            src={`data:image/png;base64,${image}`}
-            boxShadow="lg"
-            borderRadius="md"
-            w="100%"
-          />
-        ) : null}
-      </Container>
+          <Stack direction="row">
+            <Flex direction="column" spacing="4" mb="3">
+              <Wrap spacing="4" mb="4">
+                <Textarea
+                  value={prompt}
+                  onChange={(e) => updatePrompt(e.target.value)}
+                  width={"450px"}
+                  maxWidth={"450px"}
+                  height={"200px"}
+                  resize="none"
+                  variant="filled"
+                  placeholder="Enter your prompt here..."
+                />
+              </Wrap>
+              <Button onClick={(e) => generate(prompt)} colorScheme={"red"}>
+                Dream
+              </Button>
+            </Flex>
+          </Stack>
+        </Box>
+        <Box
+          margin="20px"
+          width={"600px"}
+          height={"600px"}
+          backgroundColor={"#dcdcdc"}
+          borderRadius={"10px"}
+        >
+          {loading ? (
+            <Stack>
+              <SkeletonCircle size="20" />
+              <SkeletonText mt="4" noOfLines={5} spacing="4" />
+            </Stack>
+          ) : image ? (
+            <Image
+              src={`data:image/png;base64,${image}`}
+              boxShadow="lg"
+              borderRadius="md"
+              h="100%"
+              objectFit="cover"
+            />
+          ) : null}
+        </Box>
+      </SimpleGrid>
+      <Box padding={"20px"}>
+        <Stack>
+          <Flex direction="column" spacing="4" mb="3">
+            <Heading>Links to all the models</Heading>
+            <Text fontSize="xl" mb="10">
+              <Link
+                href={
+                  "https://huggingface.co/dreamlike-art/dreamlike-diffusion-1.0"
+                }
+                isExternal
+                textDecoration="underline"
+              >
+                Dreamlike Diffusion v1 Model
+              </Link>
+              <br />
+              <Link
+                href={"https://huggingface.co/stabilityai/stable-diffusion-2"}
+                isExternal
+                textDecoration="underline"
+              >
+                Stable Diffusion v2 Model
+              </Link>
+              <br />
+              <Link
+                href={"https://huggingface.co/runwayml/stable-diffusion-v1-5"}
+                isExternal
+                textDecoration="underline"
+              >
+                Stable Diffusion v1.5 Model
+              </Link>
+              <br />
+              <Link
+                href={"https://huggingface.co/nousr/robo-diffusion"}
+                isExternal
+                textDecoration="underline"
+              >
+                Robo Diffusion
+              </Link>
+              <br />
+              <Link
+                href={"https://huggingface.co/nitrosocke/mo-di-diffusion"}
+                isExternal
+                textDecoration="underline"
+              >
+                Mo Di Diffusion Model
+              </Link>
+              <br />
+              <Link
+                href={"https://huggingface.co/hakurei/waifu-diffusion"}
+                isExternal
+                textDecoration="underline"
+              >
+                Waifu Diffusion Model
+              </Link>
+              <br />
+            </Text>
+          </Flex>
+        </Stack>
+      </Box>
     </ChakraProvider>
   );
 };
